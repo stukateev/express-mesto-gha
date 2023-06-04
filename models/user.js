dbff6bc4b7d8046ea8fa2969f90f012dfb15a675
Dmitry Stukateev <Dmitry.Stukateev@yandex.ru>
2023-06-05 01:30:38 +0300

    PR14 Second commit

--- models/user.js
+++ models/user.js
@@ -22,16 +22,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       default:
         'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-      validate: [
+      validate:
         {
           validator: (url) => reIsUrl.test(url),
-          message: 'Некорректный URL',
-        },
-        {
-          validator: (url) => validator.isURL(url),
-          message: 'Некорректный URL',
-        },
-      ],
+          message: 'Некорректный URL'
+        }
     },
     email: {
       type: String,
