0e2b044c3553cdc9c22d4bcaa98862244fa61291
Dmitry Stukateev <Dmitry.Stukateev@yandex.ru>
2023-06-05 02:16:52 +0300

    PR14 Second commit

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,6 +1,10 @@
-const errorHandler = (err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
+const UnauthorizedError = require('../utils/UnauthorizedError');
 
+const errorHandler = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  if (err instanceof UnauthorizedError) {
+    statusCode = 401;
+  }
   const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
   res.status(statusCode).send({ message });
   next();
