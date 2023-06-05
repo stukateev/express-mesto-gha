2f558e8167aa63bb35eeb455acdf65ceaa0713da
Dmitry Stukateev <Dmitry.Stukateev@yandex.ru>
2023-06-06 01:31:06 +0300

    PR14 Third commit

--- controllers/users.js
+++ controllers/users.js
@@ -89,11 +89,11 @@ const updateAvatar = (req, res, next) => {
 };
 
 const getCurrentUser = (req, res, next) => Users.findById(req.user._id)
-    .orFail()
-    .then((user) => res.send(user))
-    .catch((err) => {
-      handleError(err, next);
-    })
+  .orFail()
+  .then((user) => res.send(user))
+  .catch((err) => {
+    handleError(err, next);
+  },)
 
 const login = (req, res, next) => {
   const { email, password } = req.body;
