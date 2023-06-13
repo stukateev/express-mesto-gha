ca0e7782c8805d0845d2b1d2b5bc701aea050f5a
Dmitry Stukateev <Dmitry.Stukateev@yandex.ru>
2023-06-13 16:50:00 +0300

    fix CORS

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { errors } = require('celebrate');
 const routes = require('./routes');
 require('dotenv').config();
 const cors = require('./middlewares/cors');
-const { PORT = 80, DB_PATH = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_PATH = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
 const auth = require('./middlewares/auth');
