63dcd796d5342ca91f9c6832f09edc4f495e6944
Dmitry Stukateev <Dmitry.Stukateev@yandex.ru>
2023-06-18 20:05:54 +0300

    fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
   const  { jwt } = req.cookies
 
   if(!jwt){
-    throw next(new UnauthorizedError('Authorization required'))
+    throw next(new UnauthorizedError('Authorization requiredff'))
   }
 
   const { NODE_ENV, JWT_SECRET } = process.env;
@@ -14,13 +14,13 @@ module.exports = (req, res, next) => {
   let payload;
   try {
     payload = token.verify(
-      token,
+      jwt,
       NODE_ENV === 'production'
         ? JWT_SECRET
         : '1ce9ec7dd68836579e4ffcb80e1ea34ae6e9707c6b36a0c247e501d339a5ec0b',
     );
   } catch (err) {
-    throw new UnauthorizedError('Authorization required');
+    throw new UnauthorizedError('Authorization requireddd');
   }
 
   req.user = payload;
