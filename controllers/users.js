22cd633ddaab54066eb0f8837f79465c7aec5b17
Dmitry Stukateev <Dmitry.Stukateev@yandex.ru>
2023-06-19 02:25:26 +0300

    fix JWT

--- controllers/users.js
+++ controllers/users.js
@@ -113,6 +113,7 @@ const login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
           sameSite: "none",
+          secure: true,
         })
         .json({ message: `Welcome back, ${user.name}` })
         .send(user);
