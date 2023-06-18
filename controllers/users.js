5005d3f14c0453cab386b72a72157b77537d97ef
Dmitry Stukateev <Dmitry.Stukateev@yandex.ru>
2023-06-18 23:36:24 +0300

    fix Cors

--- controllers/users.js
+++ controllers/users.js
@@ -115,7 +115,7 @@ const login = (req, res, next) => {
           httpOnly: true,
           sameSite: true,
         })
-        .send({ token });
+        .send({ message: `Welcome back, ${user.name}` });
     })
     .catch((err) => {
       handleError(err, next);
